e71653dcb47a687491e96e1af8da2b764c488e30
Eduardo Zluhan Samrsla <eduardp.zluhan@konecty.com>
2018-07-09 17:02:16 -0300

    change icon to fontawesome on product list

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,66 +9,37 @@ import {
   Text,
   Icon
 } from 'native-base';
+import FontAwesome, { Icons } from 'react-native-fontawesome';
+
+import Layout from './Layout';
+
+// const thumb = require('../../assets/foxter.jpg');
+const thumb = {
+  uri:
+    'https://cdn-my.konecty.com/rest/image/outer/263/263/foxter/wm/Product/174493/pictures/02-CASA-CHACARA-DAS-PEDRAS-PORTO-ALEGRE-174493.jpg'
+};
 
 export default class Results extends Component {
   render() {
-    return (
-      <Container>
-        <Content>
-          <List>
-            <ListItem>
-              <Thumbnail
-                square
-                size={80}
-                source={{ uri: 'https://www.placecage.com/200/200' }}
-              />
-              <Body>
-                <Text>Imóvel 174493 - Casa Revenda</Text>
-                <Text note>Chácara das Pedras, Porto Alegre</Text>
-                  <Text>166,5m² 2
-                  <Icon
-                    type="FontAwesome"
-                    name="car"
-                    style={{ width: 20, height: 20 }}
-                  />
-                  3 
-                  <Icon
-                    type="FontAwesome"
-                    name="bed"
-                    style={{ width: 20, height: 20 }}
-                  />
-                </Text>
-                <Text>R$ 650.000</Text>
-              </Body>
-            </ListItem>
-            <ListItem>
-              <Thumbnail
-                square
-                size={80}
-                source={{ uri: 'https://www.placecage.com/200/200' }}
-              />
-              <Body>
-                <Text>Imóvel 174493 - Casa Revenda</Text>
-                <Text note>Chácara das Pedras, Porto Alegre</Text>
-                  <Text>166,5m² 2
-                  <Icon
-                    type="FontAwesome"
-                    name="car"
-                    style={{ width: 20, height: 20 }}
-                  />
-                  3 
-                  <Icon
-                    type="FontAwesome"
-                    name="bed"
-                    style={{ width: 20, height: 20 }}
-                  />
-                </Text>
-                <Text>R$ 650.000</Text>
-              </Body>
-            </ListItem>
-          </List>
-        </Content>
-      </Container>
-    );
+    return <Layout navigation={this.props.navigation}>
+        <Container>
+          <Content>
+            <List>
+              <ListItem onPress={() => this.props.navigation.navigate('Product')}>
+                <Thumbnail square size={80} source={thumb} />
+                <Body>
+                  <Text>Imóvel 174493 - Casa Revenda</Text>
+                  <Text note>Chácara das Pedras, Porto Alegre</Text>
+                  <Text>
+                    166,5m² 2 <FontAwesome>{Icons.car}</FontAwesome> 
+                    {' '}3 <FontAwesome>{Icons.bed}</FontAwesome>
+                  </Text>
+                  <Text>R$ 650.000</Text>
+                </Body>
+              </ListItem>
+            </List>
+          </Content>
+        </Container>
+      </Layout>;
   }
 }
